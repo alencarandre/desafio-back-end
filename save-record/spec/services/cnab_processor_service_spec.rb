04512e8a739dc368e3d6888fd31e85017925c572
André Alencar <andreluis88@gmail.com>
2020-09-08 01:40:25 -0300

    [Save Record] Create service to save movement from Cnab

---
+++ save-record/spec/services/cnab_processor_service_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe CnabProcessorService, type: :model do
+  describe '#call' do
+    let(:cnab) { FactoryBot.build(:cnab) }
+
+    describe 'when cnab has already processed' do
+      before do
+        allow(cnab)
+          .to receive(:transaction_hash)
+          .and_return('transaction_hash')
+        
+        FactoryBot.create(:movement, transaction_hash: 'transaction_hash')
+      end
+
+      it 'does not create new movement' do
+        expect { described_class.(cnab) }
+          .to_not change { Movement.count }
+      end
+    end
+
+    describe 'when cnab does not processed' do
+      before do
+        allow(cnab)
+          .to receive(:transaction_hash)
+          .and_return(SecureRandom.hex)
+       end
+
+      it 'creates new movement' do
+        expect { described_class.(cnab) }
+          .to change { Movement.count }
+        expect(Movement.where(transaction_hash: cnab.transaction_hash))
+          .to be_present
+      end
+    end
+
+    describe 'when transaction_type does not exist' do
+      it('raise not found error') do
+        cnab.transaction_type = -10
+
+        expect { described_class.(cnab) }
+          .to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    describe 'when store does not exist' do
+      it 'creates a store' do
+        cnab.store = Faker::Name.unique.name
+
+        expect { described_class.(cnab) }
+          .to change { Store.count }
+        expect(Store.where(name: cnab.store)).to be_present
+      end
+    end
+
+    describe 'when store exist' do
+      it 'does not create a store' do
+        cnab.store = FactoryBot.create(:store).name
+
+        expect { described_class.(cnab) }
+          .to_not change { Store.count }
+      end
+    end
+
+    describe 'when owner does not exist' do
+      it 'creates a owner' do
+        cnab.owner = Faker::Name.unique.name
+        
+        expect { described_class.(cnab) }
+          .to change { Owner.count }
+        expect(Owner.where(name: cnab.owner)).to be_present
+      end
+    end
+
+    describe 'when owner exist' do
+      it 'does not creates a owner' do
+        cnab.owner = FactoryBot.create(:owner).name
+        
+        expect { described_class.(cnab) }
+          .to_not change { Owner.count }
+        expect(Owner.where(name: cnab.owner)).to be_present
+      end
+    end
+  end
+end
