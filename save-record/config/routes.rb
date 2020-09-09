c1628034046f4004ee4e11b7789fdf860a201d7b
André Alencar <andreluis88@gmail.com>
2020-09-09 00:03:54 -0300

    [Save Record] Create movements endpoint

--- save-record/config/routes.rb
+++ save-record/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :stores, only: [:index]
+  resources :stores, only: [:index] do
+    resources :movements, only: [:index]
+  end
 end
